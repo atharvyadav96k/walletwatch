0d2ec2a4d324c09b239105161143d57e1b345124
atharvyadav96k <atharvyadav96k@gmail.com>
2024-09-03 09:42:32 +0530

    index page with login and register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Login from "./components/users/Login";
 import Register from "./components/users/Register";
+import Index from "./components/Index";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div>
       <BrowserRouter>
+        
         <Routes>
+          <Route path="/" element={<Index/>}>
+          </Route>
           <Route path="/register" element={<Register/>}/>
           <Route path="/login" element={<Login/>}/>
         </Routes>
---
+++ src/components/Index.js
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+import React from 'react'
+
+export default function Index() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <button onClick={()=>{navigate('/login')}} style={{background: "white", width: "300px"}}>Login</button>
+      <button onClick={()=>{navigate('/register')}} style={{background: "white", width: "300px"}}>Register</button>
+    </div>
+  )
+}
